396764b2e7b19184f1e709debe264ff47a8e373f
Matt Vitello <Matt@Matts-MacBook-Pro-2.local>
2023-08-13 21:42:31 -0400

    added filters

---
+++ src/components/adminPanel.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="admin--card">
+    <form id="admin-filters">
+      <fieldset>
+        <legend>Filters</legend>
+        <label>Filter by league</label>
+        <select
+          id="league-filter"
+          class="w-100"
+          @change="
+            (event) => {
+              filterByLeague(event);
+            }
+          "
+          v-model="this.leagueFilter"
+        >
+          <option value="mens">Men's</option>
+          <option value="coed">Co-Ed</option>
+          <option value="both">Both</option>
+        </select>
+
+        <label>Filter by attribute</label>
+        <select
+          id="attribute-filter"
+          class="w-100"
+          @change="
+            (event) => {
+              filterByAttributeHandler(event);
+            }
+          "
+          v-model="this.attributeFilter"
+        >
+          <option value="paid">Paid</option>
+          <option value="unpaid">Unpaid</option>
+          <option value="needsGrouping">Needs Grouping</option>
+        </select>
+        <button role="button" @click="clearFiltersHandler">
+          Clear Filters
+        </button>
+      </fieldset>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: ["adminChoices"],
+  data() {
+    return {
+      leagueFilter: null,
+      attributeFilter: null,
+    };
+  },
+  methods: {
+    filterByLeague(ev) {
+      this.$emit("filter-league", ev.target.value);
+    },
+    filterByAttributeHandler(ev) {
+      this.$emit("filter-attribute", ev.target.value);
+    },
+    clearFiltersHandler(ev) {
+      ev.preventDefault();
+      this.leagueFilter = null;
+      this.attributeFilter = null;
+      this.$emit("clear-filters");
+    },
+  },
+};
+</script>
+<style scoped>
+label {
+  margin-top: 1em;
+}
+</style>
--- src/components/listTeams.vue
+++ src/components/listTeams.vue
@@ -1,76 +1,359 @@
 <template>
-  <div class="row admin--row">
-    <div class="admin--column"><h5>Team</h5></div>
-    <div class="admin--column"><h5>Division</h5></div>
-    <div class="admin--column"><h5>Needs Grouping</h5></div>
-  </div>
-  <div class="row admin--row" v-for="team in teamsSignedUp">
-    <div class="admin--column">
-      <div class="admin--item">
-        <div class="d-flex justify-content-between">
-          <h6>{{ team.teamName }}</h6>
-          <span class="ui-info paid" v-if="team.paid">PAID</span>
-          <span class="ui-info unpaid" v-if="!team.paid">UNPAID</span>
+  <!--mens list-->
+  <div class="admin--card" v-if="this.adminChoices.renderMensList">
+    <h2 class="mb-4">Mens League</h2>
+    <div class="row admin--row">
+      <div class="admin--column"><h5>Team</h5></div>
+      <div class="admin--column"><h5>Division</h5></div>
+      <div class="admin--column"><h5>Needs Grouping</h5></div>
+    </div>
+    <div
+      class="row admin--row"
+      v-if="!this.adminChoices.isFiltering"
+      v-for="team in teamsSignedUp.mens"
+    >
+      <div
+        class="row admin--row data"
+        :data-paid="team.paid"
+        :data-needsGrouping="team.needsGrouping"
+      >
+        <div class="admin--column">
+          <div class="admin--item">
+            <div class="d-flex justify-content-between">
+              <h6>{{ team.teamName }}</h6>
+              <span class="ui-info paid" v-if="team.paid">PAID</span>
+              <span class="ui-info unpaid" v-if="!team.paid">UNPAID</span>
+            </div>
+            <div class="team">
+              <form action="">
+                <input type="hidden" name="player-names" />
+                <span v-for="player in team.players"
+                  >{{ player.first_name }} {{ player.last_name }}</span
+                >
+              </form>
+            </div>
+          </div>
         </div>
-        <div class="team">
-          <form action="">
-            <input type="hidden" name="player-names" />
-            <span v-for="player in team.players"
-              >{{ player.first_name }} {{ player.last_name }}</span
+        <div class="admin--column">
+          <div class="admin--item">
+            <span>{{ team.division }}</span>
+          </div>
+        </div>
+        <div class="admin--column">
+          <div class="admin--item">
+            <span v-if="team.needsGrouping">yes</span>
+            <span v-if="!team.needsGrouping">no</span>
+          </div>
+        </div>
+        <div class="admin--tools">
+          <ul class="dropbtn icons btn-right showLeft" @click="showAdminTools">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div class="dropdown-content">
+            <button class="close-tools" @click="closeAdminTools">
+              &times;
+            </button>
+            <ul>
+              <li>
+                <button
+                  :data-tool-target="team.id"
+                  @click="this.editTeamHandler"
+                >
+                  Edit Team
+                </button>
+              </li>
+              <li>
+                <button :data-tool-target="team.id" @click="deleteTeamHandler">
+                  Delete Team
+                </button>
+              </li>
+              <li v-if="team.needsGrouping">
+                <button :data-tool-target="team.id" @click="groupTeamHandler">
+                  Group Team
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--filtered mens-->
+    <div
+      class="row admin--row"
+      v-if="this.adminChoices.isFiltering"
+      v-for="team in this.adminChoices.filteredTeams.mens"
+    >
+      <div class="list--container">
+        <div
+          class="row admin--row data"
+          :data-paid="team.paid"
+          :data-needsGrouping="team.needsGrouping"
+        >
+          <div class="admin--column">
+            <div class="admin--item">
+              <div class="d-flex justify-content-between">
+                <h6>{{ team.teamName }}</h6>
+                <span class="ui-info paid" v-if="team.paid">PAID</span>
+                <span class="ui-info unpaid" v-if="!team.paid">UNPAID</span>
+              </div>
+              <div class="team">
+                <form action="">
+                  <input type="hidden" name="player-names" />
+                  <span v-for="player in team.players"
+                    >{{ player.first_name }} {{ player.last_name }}</span
+                  >
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="admin--column">
+            <div class="admin--item">
+              <span>{{ team.division }}</span>
+            </div>
+          </div>
+          <div class="admin--column">
+            <div class="admin--item">
+              <span v-if="team.needsGrouping">yes</span>
+              <span v-if="!team.needsGrouping">no</span>
+            </div>
+          </div>
+          <div class="admin--tools">
+            <ul
+              class="dropbtn icons btn-right showLeft"
+              @click="showAdminTools"
             >
-          </form>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <div class="dropdown-content">
+              <button class="close-tools" @click="closeAdminTools">
+                &times;
+              </button>
+              <ul>
+                <li>
+                  <button
+                    :data-tool-target="team.id"
+                    @click="this.editTeamHandler"
+                  >
+                    Edit Team
+                  </button>
+                </li>
+                <li>
+                  <button
+                    :data-tool-target="team.id"
+                    @click="deleteTeamHandler"
+                  >
+                    Delete Team
+                  </button>
+                </li>
+                <li v-if="team.needsGrouping">
+                  <button :data-tool-target="team.id" @click="groupTeamHandler">
+                    Group Team
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <!-- <div class="admin--column">
-      <div class="admin--item">
-        <span>{{ team.players }}</span>
-      </div>
-    </div> -->
-    <div class="admin--column">
-      <div class="admin--item">
-        <span>{{ team.division }}</span>
+    <div
+      class="no--data"
+      v-if="
+        this.adminChoices.isFiltering &&
+        Object.keys(this.adminChoices.filteredTeams.mens).length === 0
+      "
+    >
+      <p>No results matching your request</p>
+    </div>
+  </div>
+  <!--coed list-->
+  <div class="admin--card" v-if="this.adminChoices.renderCoedList">
+    <h2 class="mb-4">Coed League</h2>
+    <div class="row admin--row">
+      <div class="admin--column"><h5>Team</h5></div>
+      <div class="admin--column"><h5>Division</h5></div>
+      <div class="admin--column"><h5>Needs Grouping</h5></div>
+    </div>
+    <div
+      class="row admin--row"
+      v-if="!this.adminChoices.isFiltering"
+      v-for="team in teamsSignedUp.coed"
+    >
+      <div class="list--container">
+        <div
+          class="row admin--row data"
+          :data-paid="team.paid"
+          :data-needsGrouping="team.needsGrouping"
+        >
+          <div class="admin--column">
+            <div class="admin--item">
+              <div class="d-flex justify-content-between">
+                <h6>{{ team.teamName }}</h6>
+                <span class="ui-info paid" v-if="team.paid">PAID</span>
+                <span class="ui-info unpaid" v-if="!team.paid">UNPAID</span>
+              </div>
+              <div class="team">
+                <form action="">
+                  <input type="hidden" name="player-names" />
+                  <span v-for="player in team.players"
+                    >{{ player.first_name }} {{ player.last_name }}</span
+                  >
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="admin--column">
+            <div class="admin--item">
+              <span>{{ team.division }}</span>
+            </div>
+          </div>
+          <div class="admin--column">
+            <div class="admin--item">
+              <span v-if="team.needsGrouping">yes</span>
+              <span v-if="!team.needsGrouping">no</span>
+            </div>
+          </div>
+          <div class="admin--tools">
+            <ul
+              class="dropbtn icons btn-right showLeft"
+              @click="showAdminTools"
+            >
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <div class="dropdown-content">
+              <button class="close-tools" @click="closeAdminTools">
+                &times;
+              </button>
+              <ul>
+                <li>
+                  <button
+                    :data-tool-target="team.id"
+                    @click="this.editTeamHandler"
+                  >
+                    Edit Team
+                  </button>
+                </li>
+                <li>
+                  <button
+                    :data-tool-target="team.id"
+                    @click="deleteTeamHandler"
+                  >
+                    Delete Team
+                  </button>
+                </li>
+                <li v-if="team.needsGrouping">
+                  <button :data-tool-target="team.id" @click="groupTeamHandler">
+                    Group Team
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="admin--column">
-      <div class="admin--item">
-        <span v-if="team.needsGrouping">yes</span>
-        <span v-if="!team.needsGrouping">no</span>
+    <!--filtered coed-->
+    <div
+      class="row admin--row"
+      v-if="this.adminChoices.isFiltering"
+      v-for="team in this.adminChoices.filteredTeams.coed"
+    >
+      <div class="list--container">
+        <div
+          class="row admin--row data"
+          :data-paid="team.paid"
+          :data-needsGrouping="team.needsGrouping"
+        >
+          <div class="admin--column">
+            <div class="admin--item">
+              <div class="d-flex justify-content-between">
+                <h6>{{ team.teamName }}</h6>
+                <span class="ui-info paid" v-if="team.paid">PAID</span>
+                <span class="ui-info unpaid" v-if="!team.paid">UNPAID</span>
+              </div>
+              <div class="team">
+                <form action="">
+                  <input type="hidden" name="player-names" />
+                  <span v-for="player in team.players"
+                    >{{ player.first_name }} {{ player.last_name }}</span
+                  >
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="admin--column">
+            <div class="admin--item">
+              <span>{{ team.division }}</span>
+            </div>
+          </div>
+          <div class="admin--column">
+            <div class="admin--item">
+              <span v-if="team.needsGrouping">yes</span>
+              <span v-if="!team.needsGrouping">no</span>
+            </div>
+          </div>
+          <div class="admin--tools">
+            <ul
+              class="dropbtn icons btn-right showLeft"
+              @click="showAdminTools"
+            >
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <div class="dropdown-content">
+              <button class="close-tools" @click="closeAdminTools">
+                &times;
+              </button>
+              <ul>
+                <li>
+                  <button
+                    :data-tool-target="team.id"
+                    @click="this.editTeamHandler"
+                  >
+                    Edit Team
+                  </button>
+                </li>
+                <li>
+                  <button
+                    :data-tool-target="team.id"
+                    @click="deleteTeamHandler"
+                  >
+                    Delete Team
+                  </button>
+                </li>
+                <li v-if="team.needsGrouping">
+                  <button :data-tool-target="team.id" @click="groupTeamHandler">
+                    Group Team
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="admin--tools">
-      <ul class="dropbtn icons btn-right showLeft" @click="showAdminTools">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-      <div class="dropdown-content">
-        <button class="close-tools" @click="closeAdminTools">&times;</button>
-        <ul>
-          <li>
-            <button :data-tool-target="team.id" @click="this.editTeamHandler">
-              Edit Team
-            </button>
-          </li>
-          <li>
-            <button :data-tool-target="team.id" @click="deleteTeamHandler">
-              Delete Team
-            </button>
-          </li>
-          <li v-if="team.needsGrouping">
-            <button :data-tool-target="team.id" @click="groupTeamHandler">
-              Group Team
-            </button>
-          </li>
-        </ul>
-      </div>
+    <div
+      class="no--data"
+      v-if="
+        this.adminChoices.isFiltering &&
+        Object.keys(this.adminChoices.filteredTeams.coed).length === 0
+      "
+    >
+      <p>No results matching your request</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["teamsSignedUp"],
+  props: ["teamsSignedUp", "adminChoices"],
   methods: {
     closeAdminTools(ev) {
       const parent = ev.target.parentNode;
@@ -113,6 +396,10 @@ h6 {
   font-weight: bold;
   display: inline;
 }
+.admin--row,
+.list--container {
+  width: 100%;
+}
 .admin--column:first-child {
   flex-basis: 50%;
 }
@@ -121,5 +408,9 @@ h6 {
 }
 .team {
   font-size: 0.95rem;
+}
+.no--data {
+  margin: 2em auto;
+  text-align: center;
 }
 </style>
