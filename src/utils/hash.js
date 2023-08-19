9642e3fb8bc2e82d5452b9ee6a0e0d54fd3e280c
Matt Vitello <Matt@Matts-MacBook-Pro-2.local>
2023-08-18 23:32:47 -0400

    hashing attempt

---
+++ src/utils/hash.js
@@ -0,0 +1,4 @@
+"use strict";
+const hash = Math.floor(Math.random() * 90000) + 10000;
+
+export { hash }
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { hash } from "./src/utils/hash";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
